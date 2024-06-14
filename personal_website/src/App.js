d3b898e0d91e8ec1f56457029b4522d727656840
rooja13 <rbarajas1633@yahoo.com>
2024-06-14 15:00:24 -0700

    Basic Project Layout Setup

---
+++ personal_website/src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import Homepage from './components/homepage.js';
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <Homepage/>
+    </div>
+  );
+}
+
+export default App;
---
+++ personal_website/src/components/homepage.js
@@ -0,0 +1,13 @@
+import './homepage.css';
+
+function Homepage(){
+    return(
+        <>
+            <header>
+                <h1>This is a Header!</h1>
+            </header>
+        </>
+    )
+}
+
+export default Homepage;
